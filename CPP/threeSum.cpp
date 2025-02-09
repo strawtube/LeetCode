9126eb4329f8a1caec86db255c12e0c6d022397e
Syeeda Aiemen Dania Saleem <119602615+strawtube@users.noreply.github.com>
2025-02-09 19:24:47 +0530

    solved threeSum with TC O(n²)

--- CPP/threeSum.cpp
+++ CPP/threeSum.cpp
@@ -49,3 +49,38 @@ public:
         return vector<vector<int>>(sol_set.begin(), sol_set.end());
     }
 };
+
+// Time Complexity = O(n²)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int size = nums.size();
+        vector<vector<int>> sol;
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<size-2; i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            
+            int left=i+1;
+            int right=size-1;
+
+            while(left<right){
+                if(nums[i]+nums[left]+nums[right]==0){
+                    sol.push_back({nums[i], nums[left], nums[right]});
+                    while(left<right && nums[left]==nums[left+1])
+                        left++;
+                    while(left<right && nums[right]==nums[right-1])
+                        right--;
+                    left++;
+                    right--;
+                }
+                else if(nums[i]+nums[left]+nums[right]<0)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return sol;
+    }
+};

b7e0e59703420bbdd6e2a91be5a28d70b0dc2e47
Syeeda Aiemen Dania Saleem <119602615+strawtube@users.noreply.github.com>
2025-03-16 23:57:41 +0530

    solved findKthLargest in cpp

--- CPP/findKthLargest.cpp
+++ CPP/findKthLargest.cpp
@@ -61,3 +61,18 @@ private:
         return mid;
     }
 };
+
+// minHeap approach - Accepted Solution
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for(int num:nums) {
+            minHeap.push(num);
+            if(minHeap.size()>k){
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
